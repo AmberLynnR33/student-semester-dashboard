7fbcecaf868457e5af3b16157c9deb4a61e41bd9
AmberLynnR33 <103320792+AmberLynnR33@users.noreply.github.com>
2025-04-25 09:47:19 -0400

    Layout of main page

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,39 +6,61 @@ import datetime
 # config
 st.set_page_config(layout="wide")
 
+is_file_loaded = False
+
 # values to preset
-userFile, degreeList, courseList, todoList = None, [], [], []
+# userFile, degreeList, courseList, todoList = None, [], [], []
 
-try:
-    userFile = open("tester.txt", "r")
-    curReading = 0
-    curLine = -1
-    for line in userFile:
-        curLine += 1
+# try:
+#     userFile = open("tester.txt", "r")
+#     curReading = 0
+#     curLine = -1
+#     for line in userFile:
+#         curLine += 1
 
-        # switch type
-        if line.startswith("degreeList"):
-            curReading = 1
-        elif line.startswith("courseList"):
-            curReading = 2
-        elif line.startswith("todoList"):
-            curReading = 3
-        elif curLine == 0:
-            raise Exception(curLine)
+#         # switch type
+#         if line.startswith("degreeList"):
+#             curReading = 1
+#         elif line.startswith("courseList"):
+#             curReading = 2
+#         elif line.startswith("todoList"):
+#             curReading = 3
+#         elif curLine == 0:
+#             raise Exception(curLine)
         
-        splitLine = line.split(",")
+#         splitLine = line.split(",")
         
-        if curReading == 1:
-            degreeList.append(splitLine)
-        elif curReading == 2:
-            courseList.append(splitLine)
-        elif curReading == 3:
-            todoList.append(splitLine)
+#         if curReading == 1:
+#             degreeList.append(splitLine)
+#         elif curReading == 2:
+#             courseList.append(splitLine)
+#         elif curReading == 3:
+#             todoList.append(splitLine)
         
 
         
-except:
-    st.markdown("Issue loading file")
+# except:
+#     st.markdown("Issue loading file")
 
 
-st.title("Hello!")
+# MAIN PAGE
+
+if not is_file_loaded:
+    st.title("One Dashboard, All your planning needs.")
+    st.divider()
+
+    col1, col2 = st.columns([1,3], border=True)
+
+    if col1.button("Continue Your Semester", use_container_width=True):
+        is_file_loaded = True
+        st.write("hi")
+    if col1.button("Start a New Semester", use_container_width=True):
+        is_file_loaded = True
+    if col1.button("Start Anew", use_container_width=True):
+        is_file_loaded = True
+    
+    col2.markdown("Upload your :blue-background[semester.txt] file to continue planning this semester.")
+    col2.markdown("Upload your :blue-background[semester.txt] file. Start a new semester plan, keeping your grade and degree trackers in tact.")
+    col2.markdown("Start a completely blank dashboard.")
+else:
+    st.write("Hi!")
