6e0d5318497bc57fea3087da47c9de2f8934bced
AmberLynnR33 <103320792+AmberLynnR33@users.noreply.github.com>
2025-05-09 10:50:57 -0400

    Functionality of main page to dashboards

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,9 @@ import datetime
 # config
 st.set_page_config(layout="wide")
 
-is_file_loaded = False
+if "is_file_loaded" not in st.session_state:
+    st.session_state.is_file_loaded = False
+    st.session_state.loaded_dashboard = False
 
 # values to preset
 # userFile, degreeList, courseList, todoList = None, [], [], []
@@ -45,22 +47,33 @@ is_file_loaded = False
 
 # MAIN PAGE
 
-if not is_file_loaded:
+if not st.session_state.is_file_loaded:
     st.title("One Dashboard, All your planning needs.")
     st.divider()
 
     col1, col2 = st.columns([1,3], border=True)
 
     if col1.button("Continue Your Semester", use_container_width=True):
-        is_file_loaded = True
-        st.write("hi")
+        st.session_state.is_file_loaded = True
     if col1.button("Start a New Semester", use_container_width=True):
-        is_file_loaded = True
+        st.session_state.is_file_loaded = True
     if col1.button("Start Anew", use_container_width=True):
-        is_file_loaded = True
+        st.session_state.is_file_loaded = True
     
     col2.markdown("Upload your :blue-background[semester.txt] file to continue planning this semester.")
     col2.markdown("Upload your :blue-background[semester.txt] file. Start a new semester plan, keeping your grade and degree trackers in tact.")
     col2.markdown("Start a completely blank dashboard.")
 else:
-    st.write("Hi!")
+    if st.sidebar.button("Your Dashboard", use_container_width=True) or (not st.session_state.loaded_dashboard):
+        st.session_state.loaded_dashboard = True
+        st.write("HELLO!")
+        pass
+    if st.sidebar.button("Your Grades", use_container_width=True):
+        pass
+    if st.sidebar.button("Your Degree", use_container_width=True):
+        pass
+    if st.sidebar.button("About", use_container_width=True):
+        pass
+    if st.sidebar.button("**Download Your File**", use_container_width=True):
+        pass
+    
