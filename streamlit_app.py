73a7feb1f7f66d8f318b852f4f63eb372bd13610
AmberLynnR33 <103320792+AmberLynnR33@users.noreply.github.com>
2025-06-06 13:24:57 -0400

    Modified how page loads

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,12 @@ import datetime
 st.set_page_config(layout="wide")
 
 if "is_file_loaded" not in st.session_state:
-    st.session_state.is_file_loaded = False
-    st.session_state.loaded_dashboard = False
+    st.session_state.is_file_loaded = 0
+    st.session_state.loaded_dashboard = 0
+
+def set_state(i):
+    st.session_state.is_file_loaded = i
+
 
 # values to preset
 # userFile, degreeList, courseList, todoList = None, [], [], []
@@ -47,22 +51,30 @@ if "is_file_loaded" not in st.session_state:
 
 # MAIN PAGE
 
-if not st.session_state.is_file_loaded:
+if st.session_state.is_file_loaded == 0:
     st.title("One Dashboard, All your planning needs.")
     st.divider()
 
+    st.session_state.test_df =pd.read_csv('tester_csv/example_spreadsheet.csv')
+    st.session_state.test_df.replace('', np.nan, inplace=True) 
+    st.session_state.dashboard = st.session_state.test_df[['item', 'course', 'status', 'content_type', 'due', 'grade', 'weight']]
+    st.session_state.dashboard.dropna(axis=0, how='all', inplace=True)
+    st.session_state.courses = st.session_state.test_df[['courses', 'courses_cred_num', 'courses_grades']]
+    st.session_state.courses.dropna(axis=0, how='all', inplace=True)
+    st.session_state.degree =  st.session_state.test_df[['programs_req', 'programs_name', 'programs_cred_needed', 'programs_courses_apply']]
+    st.session_state.degree.dropna(axis=0, how='all', inplace=True)
+
     col1, col2 = st.columns([1,3], border=True)
 
-    if col1.button("Continue Your Semester", use_container_width=True):
-        st.session_state.is_file_loaded = True
-    if col1.button("Start a New Semester", use_container_width=True):
-        st.session_state.is_file_loaded = True
-    if col1.button("Start Anew", use_container_width=True):
-        st.session_state.is_file_loaded = True
+    col1.button("Continue Your Semester", use_container_width=True, on_click=set_state, args=[1])
+    col1.button("Start a New Semester", use_container_width=True)
+    col1.button("Start Anew", use_container_width=True, on_click=set_state, args=[1])
     
     col2.markdown("Upload your :blue-background[semester.txt] file to continue planning this semester.")
     col2.markdown("Upload your :blue-background[semester.txt] file. Start a new semester plan, keeping your grade and degree trackers in tact.")
     col2.markdown("Start a completely blank dashboard.")
+elif st.session_state.is_file_loaded < 0:
+    st.write("Error Loading FIle")
 else:
     if st.sidebar.button("Your Dashboard", use_container_width=True) or (not st.session_state.loaded_dashboard):
         st.session_state.loaded_dashboard = True
@@ -76,4 +88,4 @@ else:
         pass
     if st.sidebar.button("**Download Your File**", use_container_width=True):
         pass
-    
+
